f0a3f1567ee0d3e2c254ee707e1b2dcf3f42cf33
PegahNa <pegah_uk@yahoo.co.uk>
2023-07-17 23:06:29 +0100

    test for bankAccount added

--- bankAccount.js
+++ bankAccount.js
@@ -1,27 +1,34 @@
 // This file will contain the BankAccount class, which handles the account's
 // transactions and balances. It will import and utilize the Transaction class.
+
+// Import the necessary modules
 const Transaction = require("./transaction");
 const StatementPrinter = require("./statementPrinter");
 
+// Define the BankAccount class
 class BankAccount {
   constructor() {
     this.transactions = [];
     this.statementPrinter = new StatementPrinter();
   }
 
+  // Method to add a deposit transaction
   addDeposit(date, amount) {
     this.addTransaction(date, amount, "deposit");
   }
 
+  // Method to add a withdrawal transaction
   addWithdrawal(date, amount) {
     this.addTransaction(date, amount, "withdrawal");
   }
 
+  // Method to add a transaction to the transaction list
   addTransaction(date, amount, type) {
     const transaction = new Transaction(date, amount, type);
     this.transactions.push(transaction);
   }
 
+  // Method to calculate the current balance based on the transactions
   calculateBalance() {
     let balance = 0;
     for (const transaction of this.transactions) {
@@ -34,12 +41,20 @@ class BankAccount {
     return balance;
   }
 
+  // Method to print the account statement
   printStatement() {
+    // Check if there are no transactions
+    if (this.transactions.length === 0) {
+      return "No transactions found";
+    }
+
     const statement = this.statementPrinter.generateStatement(
       this.transactions
     );
     console.log(statement);
+    return statement;
   }
 }
 
+// Export the BankAccount class
 module.exports = BankAccount;
--- tests/bankAccount.test.js
+++ tests/bankAccount.test.js
@@ -1,17 +1,51 @@
+// Import the BankAccount class
 const BankAccount = require("../bankAccount");
 
+// Test suite for the BankAccount class
 describe("BankAccount", () => {
   let bankAccount;
 
+  // Before each test, create a new BankAccount instance
   beforeEach(() => {
     bankAccount = new BankAccount();
   });
 
+  // Test case: should calculate the correct balance after adding transactions
   test("should calculate the correct balance after adding transactions", () => {
     bankAccount.addDeposit("10/01/2023", 1000);
     bankAccount.addDeposit("13/01/2023", 2000);
-    bankAccount.addWithdrawal("14/01/2023", 500);
 
-    expect(bankAccount.calculateBalance()).toBe(2500);
+    expect(bankAccount.calculateBalance()).toBe(3000);
+  });
+
+  // Test case: should calculate the correct balance after subtracting transaction
+  test("should calculate the correct balance after subtracting transaction", () => {
+    bankAccount.addDeposit("10/01/2023", 1000);
+    bankAccount.addWithdrawal("13/01/2023", 500);
+    bankAccount.addWithdrawal("15/01/2023", 200);
+
+    expect(bankAccount.calculateBalance()).toBe(300);
+  });
+
+  // Test case: should calculate the correct balance with a mix of deposits and withdrawals
+  test("should calculate the correct balance with a mix of deposits and withdrawals", () => {
+    bankAccount.addDeposit("10/01/2023", 1000);
+    bankAccount.addWithdrawal("13/01/2023", 500);
+    bankAccount.addDeposit("15/01/2023", 2000);
+    bankAccount.addWithdrawal("17/01/2023", 800);
+
+    expect(bankAccount.calculateBalance()).toBe(1700);
+  });
+
+  // Test case: should calculate the correct balance with no transactions
+  test("should calculate the correct balance with no transactions", () => {
+    expect(bankAccount.calculateBalance()).toBe(0);
+  });
+
+  // Test case: should print an empty statement when there are no transactions
+  test("should print an empty statement when there are no transactions", () => {
+    const statement = bankAccount.printStatement();
+
+    expect(statement).toBe("No transactions found");
   });
 });
