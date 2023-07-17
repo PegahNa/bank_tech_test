433bca6832138ed8f047bba7ac73ea46179c6861
PegahNa <pegah_uk@yahoo.co.uk>
2023-07-17 18:45:04 +0100

    modified the bankAccount cladd to implement calculateBalance

--- bankAccount.js
+++ bankAccount.js
@@ -1,10 +1,20 @@
 // This file will contain the BankAccount class, which handles the account's
 // transactions and balances. It will import and utilize the Transaction class.
 const Transaction = require("./transaction");
+const StatementPrinter = require("./statementPrinter");
 
 class BankAccount {
   constructor() {
     this.transaction = [];
+    this.statementPrinter = new StatementPrinter();
+  }
+
+  addDeposit(date, amount) {
+    this.addTransaction(date, amount, "deposit");
+  }
+
+  addWithdrawal(date, amount) {
+    this.addTransaction(date, amount, "withdrawal");
   }
 
   addTransaction(date, amount, type) {
@@ -22,6 +32,13 @@ class BankAccount {
       }
     }
     return balance;
+  }
+
+  printStatement() {
+    const statement = this.statementPrinter.generateStatement(
+      this.transactions
+    );
+    console.log(statement);
   }
 }
 
