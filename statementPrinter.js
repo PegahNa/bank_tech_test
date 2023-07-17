2063d8e4052567b1d328adf1282d27a47f88e18e
PegahNa <pegah_uk@yahoo.co.uk>
2023-07-17 18:13:06 +0100

    StatementPrinter class added

--- statementPrinter.js
+++ statementPrinter.js
@@ -1,3 +1,10 @@
 // This file will contain the StatementPrinter class,
 // which generates the account statement.
 // It will import and utilize the Transaction and BankAccount classes.
+const BankAccount = require("./bankAccount");
+
+class StatementPrinter {
+  constructor() {
+    this.bankAccount = new BankAccount();
+  }
+}
