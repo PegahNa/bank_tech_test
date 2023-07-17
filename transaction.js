a759058e3a1f627f0dfc9e5b1aa18eb4498b3193
PegahNa <pegah_uk@yahoo.co.uk>
2023-07-17 17:56:25 +0100

    Transaction class added

--- transaction.js
+++ transaction.js
@@ -1,2 +1,10 @@
 // This file will contain the Transaction class,
 // which represents an individual deposit or withdrawal.
+class Transaction {
+  constructor(description, amount) {
+    this.description = description;
+    this.amount = amount;
+  }
+}
+
+module.exports = Transaction;
